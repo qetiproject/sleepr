205a52a385109ecce7bb4ba62dd55eb401e112d5
qetiproject <ketideveloper@gmail.com>
2026-04-17 13:05:51 +0400

    config, database modules

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  providers: [CommonService],
-  exports: [CommonService],
+  imports: [DatabaseModule, ConfigModule],
 })
 export class CommonModule {}
